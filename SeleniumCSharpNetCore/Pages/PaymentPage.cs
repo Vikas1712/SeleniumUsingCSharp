043b5946e051f38d7d0f1f57ce5efb9d1bfb66da
Vikas Yadav <vikas.a.yadav@capgemini.com>
2022-06-20 19:10:11 +0200

    Payment Page and deleted old features folder

---
+++ SeleniumCSharpNetCore/Pages/PaymentPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumCSharpNetCore.Pages
+{
+    public class PaymentPage
+    {
+        private IWebDriver _driver;
+        private WaitActionPage _waitActions;
+
+        public PaymentPage(IWebDriver driver)
+        {
+            _driver = driver;
+            _waitActions = new WaitActionPage(driver);
+        }
+
+        private readonly By btnPayBycheck = By.CssSelector("a[title='Pay by check.']");
+        private readonly By btnIConfirmMyOrder = By.CssSelector("button[class='button btn btn-default button-medium'] span");
+        private readonly By txtOrderConfirmation = By.CssSelector(".page-heading");
+        private readonly By alertOrderConfirmation = By.CssSelector(".alert.alert-success");
+
+        public void MakePaymentbyCheck()
+        {
+            _waitActions.WaitForElementClickable(btnPayBycheck);
+            _waitActions.ClickElement(btnPayBycheck);
+        }
+        public void SelectIConfirmMyOrder()
+        {
+            _waitActions.WaitForElementClickable(btnIConfirmMyOrder);
+            _waitActions.ClickElement(btnIConfirmMyOrder);
+        }
+        public void VerifyOrderPlaceSuccessfully()
+        {
+            _waitActions.WaitForElementDisplayed(txtOrderConfirmation);
+            _waitActions.WaitForElementDisplayed(alertOrderConfirmation);
+        }
+    }
+}
---
+++ SeleniumCSharpNetCore/Pages/ShipppingPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumCSharpNetCore.Pages
+{
+    public class ShipppingPage
+    {
+        private IWebDriver _driver;
+        private WaitActionPage _waitActions;
+
+        public ShipppingPage(IWebDriver driver)
+        {
+            _driver = driver;
+            _waitActions = new WaitActionPage(driver);
+        }
+
+        private readonly By checkBoxTermsAndConition = By.CssSelector("#cgv");
+        private readonly By btnShippingProceedToCheckout = By.CssSelector("button[name = 'processCarrier'] span");
+        
+        public void AgreeTermsAndContions()
+        {
+            _driver.FindElement(checkBoxTermsAndConition).Click();
+        }
+
+        public void SelectProceedToCheckout()
+        {
+            _waitActions.WaitForElementClickable(btnShippingProceedToCheckout);
+            _waitActions.ClickElement(btnShippingProceedToCheckout);
+        }
+    }
+}
