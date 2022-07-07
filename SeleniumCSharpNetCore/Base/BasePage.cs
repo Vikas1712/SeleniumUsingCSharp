7f07c86a30daef8f916b18dbd2b9327bc76e9156
Vikas Yadav <dnsvikas.wins@gmail.com>
2022-07-08 00:11:28 +0200

    Added comment to the files

--- SeleniumCSharpNetCore/Base/BasePage.cs
+++ SeleniumCSharpNetCore/Base/BasePage.cs
@@ -1,4 +1,12 @@
-﻿namespace SeleniumCSharpNetCore.Base
+﻿//ConfigReader file read the custom XML file 
+//We are using System.XML namespace and will be using 
+//XPathItem and XPathDocument classes to perform operations.
+//Instead of initializing a page in each class we created a base abstract class. 
+//We are using Generic type parameter T which make it possible to design classes and method 
+//that defer the specification of one or more types until the 
+//classes and method is declared and instantiated by code
+
+namespace SeleniumCSharpNetCore.Base
 {
     public abstract class BasePage:Base
     {
